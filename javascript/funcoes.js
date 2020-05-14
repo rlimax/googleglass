9384f96941e37edc50fa4e05bf37653043e3cdaa
rlimax <rodrigoetb@gmail.com>
2020-05-14 19:23:30 -0300

    Commit - Parte 9: Aulas 33, 34 e 36 - Formulário parte 1 | Formulário parte 2 | Formulário e Javascript.

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -2,3 +2,9 @@
 function mudaFoto(foto){
 	document.getElementById("icone").src = foto+".png";
 }
+
+function calcTotal(){
+	var qtd= parseInt(document.getElementById('cQtd').value);
+	tot = qtd*1500;
+	document.getElementById('cTot').value = tot;
+}
